8bf0ae4095ecc996fb27530eeb9d264c8c4c92ac
AkaemNl <Begic.nail97@outlook.fr>
2026-05-04 00:21:23 +0200

    ajout menu et protection division par zero

--- calcul.c
+++ calcul.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-void calculer(float a, float b)
-
+int main(void)
 {
-    printf("Addition: %.2f\n", a + b);
-    printf("Soustraction: %.2f\n", a - b);
-    printf("Multiplication: %.2f\n", a * b);
-    printf("Division: %.2f\n", a / b);
-}
-    int main (void)
-    {
-      float a;
-      float b;
-    printf("Entrez le premier nombre: ");
+    float a;
+    float b;
+    int choix;
+
+    printf("1. Addition\n");
+    printf("2. Soustraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Choisis : ");
+    scanf("%d", &choix);
+
+    printf("Premier nombre : ");
     scanf("%f", &a);
-    printf("Entrez le deuxième nombre: ");
+    printf("Deuxième nombre : ");
     scanf("%f", &b);
-    calculer(a, b);
-    return 0;
+
+    if (choix == 1)
+        printf("Résultat : %.2f\n", a + b);
+    else if (choix == 2)
+        printf("Résultat : %.2f\n", a - b);
+    else if (choix == 3)
+        printf("Résultat : %.2f\n", a * b);
+    else if (choix == 4)
+    {
+        if (b != 0)
+            printf("Résultat : %.2f\n", a / b);
+        else
+            printf("Erreur : Division par zéro !\n");
     }
- 
+    else
+        printf("Choix invalide\n");
+
+    return 0;
+}
