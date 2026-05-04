edf8bf0d5b138316c1bbab173f6bbf302aaf7754
AkaemNl <Begic.nail97@outlook.fr>
2026-05-05 00:30:48 +0200

    exercice echange avec pointeurs

--- hello.c
+++ hello.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+void exchange(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main(void)
 {
-   int x = 10;
-   int y = 20;
-
-   printf("Valeur de x : %d\n", x);
-   printf("Adresse de x : %p\n", &x);
-   printf("Valeur de y : %d\n", y);
-   printf("Adresse de y : %p\n", &y);
-   int *p = &x;
-   *p = 50;
-   printf("Nouvelle valeur de x : %d\n", x);
-
-}  
+    int x = 10;
+    int y = 20;
+    
+    int *px = &x;
+    int *py = &y;
+    exchange(px, py);
+    printf("x = %d, y = %d\n", x, y);
+    printf("*px = %d, *py = %d\n", *px , *py);
+    return 0;
+}
